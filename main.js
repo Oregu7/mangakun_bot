1bcaf905f5e84932d4de7ddd203a5cfd6c43f9df
Oregu7 <jekkicool@yandex.ru>
2018-03-20 18:18:05 +0300

    add webhook

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+const fs = require("fs");
+const ip = require("ip");
 const bot = require("./app/bot");
 //start bot
-bot.startPolling();
+if (process.env.NODE_ENV === "development")
+    bot.startPolling();
+else {
+    const port = 8443;
+    // TLS options
+    const tlsOptions = {
+        key: fs.readFileSync("./webhook_pkey.pem"),
+        cert: fs.readFileSync("./webhook_cert.pem"),
+    };
+
+    // Set telegram webhook
+    bot.telegram.setWebhook(`https://${ip.address()}:${port}/bot-mangakun`, {
+        source: fs.readFileSync("./webhook_cert.pem"),
+    });
+
+    // Start https webhook
+    bot.startWebhook("/bot-mangakun", tlsOptions, port);
+}
